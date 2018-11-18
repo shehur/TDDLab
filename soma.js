9dbb3de5f6be3a2dfcfd3944b698c59abdc39de0
shehur <shehur5727@gmail.com>
2018-11-18 15:04:44 -0200

    implementação da função somar

---
+++ soma.js
@@ -0,0 +1,6 @@
+function somar(numero1, numero2) {
+	var resultado = numero1 + numero2;
+	return resultado;
+}
+
+module.exports = { somar }
--- test/teste.js
+++ test/teste.js
@@ -1,5 +1,6 @@
 const { expect } = require('code');
 const { it } = exports.lab = require('lab').script();
+const { somar } = require('../soma');
 
 it('2 + 3 deve ser 5', () => {
 	expect(somar(2, 3)).to.equal(5);
